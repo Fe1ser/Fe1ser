13fb108af3de07c389a9f11ef7a6bfe31a063b3c
Fe1ser <136340109+Fe1ser@users.noreply.github.com>
2024-04-17 10:21:41 +0300

    Create webpack.config.js

---
+++ src/pages/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+
+// Функція для отримання списку файлів у директорії
+function generateHtmlPlugins(templateDir) {
+    // Отримуємо список файлів у директорії
+    const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
+    return templateFiles.map(item => {
+        // Розділяємо ім'я файлу та розширення
+        const parts = item.split('.');
+        const name = parts[0];
+        // Створюємо новий екземпляр плагіна з поточним шляхом та ім'ям файлу
+        return new HtmlWebpackPlugin({
+            filename: `${name}.html`,
+            template: path.resolve(__dirname, `${templateDir}/${name}.html`),
+        });
+    });
+}
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    plugins: [
+        // Передаємо шлях до директорії з сторінками в функцію
+        ...generateHtmlPlugins('./src/pages')
+    ],
+};
